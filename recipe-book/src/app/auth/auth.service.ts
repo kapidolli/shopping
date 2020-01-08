b787089e3b50c7b5e1a539b66a9ac7199c86b04f
kapidolli <jasin.shala.jsh@gmail.com>
2020-01-08 19:54:51 +0100

    Polishing

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap, throwIfEmpty } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 export interface AuthResponseData {
     kind: string;
@@ -25,7 +26,7 @@ export class AuthService {
     signUp(email: string, password: string) {
         return this.http
             .post<AuthResponseData>(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCSlS-Pmb3d1BUjDw73me8XrhIbk3T03j8',
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
                 {
                     email: email,
                     password: password,
@@ -48,7 +49,7 @@ export class AuthService {
     login(email: string, password: string) {
         return this.http
             .post<AuthResponseData>(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCSlS-Pmb3d1BUjDw73me8XrhIbk3T03j8',
+                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key' + environment.firebaseAPIKey,
                 {
                     email: email,
                     password: password,
---
+++ recipe-book/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyCSlS-Pmb3d1BUjDw73me8XrhIbk3T03j8'
+};
